ef3d55cb9620375afa15faa6126867c6a360e456
YoonChangJoon <ycjun1209@gmail.com>
2026-04-09 20:17:47 +0900

    QuestData List를 배열로 수정, Egg 보상객체를 배열로 수정

--- Assets/Project/Script/Data/MagicalEgg/MagicalEggData.cs
+++ Assets/Project/Script/Data/MagicalEgg/MagicalEggData.cs
@@ -1,4 +1,5 @@
 
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MagicalEggData : BaseData_ForUi
@@ -6,5 +7,5 @@ public class MagicalEggData : BaseData_ForUi
     // 알 상호작용으로 열리는 퀘스트창에 전달
     [SerializeField] protected int[] questIndexs;
 
-    [SerializeField] protected BaseReward reward;
+    [SerializeField] protected BaseReward[] reward;
 }
--- Assets/Project/Script/Data/Quest/QuestData.cs
+++ Assets/Project/Script/Data/Quest/QuestData.cs
@@ -10,24 +10,24 @@ public enum QuestType
     Sub, // 플레이어 선택
 }
 
-
+[CreateAssetMenu(fileName = "QuestData",menuName ="Quest/QuestData")]
 public class QuestData : BaseData_ForUi
 {
     [Header("Quest Identity")]
     public QuestType questType;
 
     [Header("Requirements (수락 조건)")]
-    public List<int> prerequisiteQuestIDs; // 선행 퀘스트 ID 목록
+    public int[] prerequisiteQuestIDs; // 선행 퀘스트 ID 목록
 
     [Header("Progress (진행 및 완료)")]
-    public List<BaseQuestCondition> completionConditions;   // 완료 조건 (몬스터 처치, 아이템 수집 등)
-    public List<BaseQuestCondition> additionalConditions;   // 추가보상 조건
-    public List<BaseQuestCondition> failureConditions;    // 실패 조건 (특정 NPC 사망 등)
+    public BaseQuestCondition[] completionConditions;   // 완료 조건 (몬스터 처치, 아이템 수집 등)
+    public BaseQuestCondition[] additionalConditions;   // 추가보상 조건
+    public BaseQuestCondition[] failureConditions;    // 실패 조건 (특정 NPC 사망 등)
 
     [Header("Rewards (보상)")]
-    public List<BaseQuestReward> baseRewards;           // 확정 보상
-    public List<BaseQuestReward> additionalRewards;     // 추가조건 만족시 보상
-    public List<BaseQuestReward> optionalRewards; // 선택 보상
+    public BaseQuestReward[] baseRewards;           // 확정 보상
+    public BaseQuestReward[] additionalRewards;     // 추가조건 만족시 보상
+    public BaseQuestReward[] optionalRewards; // 선택 보상
 
     [Header("World")]
     public int MapId = -1;   // Map 위치 (MapId < 0 이면 미니맵 표시 x)
